e10fe2bd4669ac7fda68a2cde559ebd0c9a3726c
AjayDyavathi <ajaydyavathi@gmail.com>
2023-10-18 01:17:58 -0400

    torch compatibilty, iu correction

--- metrics.py
+++ metrics.py
@@ -11,6 +11,7 @@ Running score adapted from meeetps and wkentaro
 https://github.com/meetps/pytorch-semseg
 https://github.com/wkentaro/pytorch-fcn/blob/master/torchfcn/utils.py
 """
+import torch
 import numpy as np
 
 
@@ -32,6 +33,12 @@ class RunningScore():
 
     def update(self, labels, preds):
         "Computes confusion matrix over a batch"
+        if isinstance(labels, torch.Tensor):
+            labels = labels.cpu().numpy()
+
+        if isinstance(preds, torch.Tensor):
+            preds = preds.cpu().numpy()
+
         for lbl, pred in zip(labels, preds):
             self.confusion_matrix += self._fast_hist(lbl.flatten(),
                                                      pred.flatten())
@@ -43,21 +50,23 @@ class RunningScore():
             - Mean IoU (float)
             - Frequency weighted accuracy (float)
         """
-        conf_mx = self.confusion_matrix
-        accuracy = conf_mx.diagonal().sum() / conf_mx.sum()
-        acc_class = conf_mx.diagonal() / conf_mx.sum(axis=1)
-        acc_class_mean = np.nanmean(acc_class)
-        intersection = conf_mx.diagonal().sum()
-        union = conf_mx.sum(axis=0) + conf_mx.sum(axis=1) - intersection
-        iu = intersection / union
-        mean_iu = np.nanmean(iu)
-        freq = conf_mx.sum(axis=1) / conf_mx.sum()
-        fwav = (freq[freq > 0] * iu[freq > 0]).sum()
-        class_iu = dict(zip(range(self.n_classes), iu))
+        # diag and sum(ax=1) can sometimes have 0s, ignore 0.0/0.0 error
+        with np.errstate(divide="ignore", invalid="ignore"):
+            conf_mx = self.confusion_matrix
+            accuracy = conf_mx.diagonal().sum() / conf_mx.sum()
+            acc_class = conf_mx.diagonal() / conf_mx.sum(axis=1)
+            acc_class_mean = np.nanmean(acc_class)
+            intersection = conf_mx.diagonal()
+            union = conf_mx.sum(axis=0) + conf_mx.sum(axis=1) - intersection
+            iu = intersection / union
+            mean_iu = np.nanmean(iu)
+            freq = conf_mx.sum(axis=1) / conf_mx.sum()
+            fwav = (freq[freq > 0] * iu[freq > 0]).sum()
+            class_iu = dict(zip(range(self.n_classes), iu))
         score_dict = {
             "Accuracy": accuracy,
             "Mean Accuracy": acc_class_mean,
-            "FreqW Acc": fwav,
+            "Global IoU": fwav,
             "Mean IoU": mean_iu
         }
         return score_dict, class_iu
